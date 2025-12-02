b0e0fea64c8c16e0fce8de5de2805c01fe456571
Adam <nestletycinka@gmail.com>
2025-12-02 22:51:32 +0100

    TTask 17 zaciatok

---
+++ Cvika17/Cvika17/MainForm.Designer.cs
@@ -0,0 +1,44 @@
+﻿
+partial class MainForm
+{
+	private System.ComponentModel.IContainer components = null;
+	private System.Windows.Forms.Timer timer1;
+
+	protected override void Dispose(bool disposing)
+	{
+		if (disposing && components != null) 
+		{
+			components.Dispose();
+		}
+		base.Dispose(disposing);
+	}
+
+	private void InitializeComponent()
+	{
+		this.components = new System.ComponentModel.Container();
+		this.timer1 = new System.Windows.Forms.Timer(this.components);
+		this.SuspendLayout();
+		// 
+		// timer1
+		// 
+		this.timer1.Enabled = true;
+		this.timer1.Tick += new System.EventHandler(this.Timer1Tick);
+		// 
+		// MainForm
+		// 
+		this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+		this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+		this.ClientSize = new System.Drawing.Size(972, 524);
+		this.Name = "MainForm";
+		this.Text = "Cvika17";
+		this.Load += new System.EventHandler(this.MainFormLoad);
+		this.Paint += new System.Windows.Forms.PaintEventHandler(this.MainFormPaint);
+		this.ResumeLayout(false);
+
+	}
+
+	public MainForm()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Cvika17/Cvika17/MainForm.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.IO;
+using System.Drawing;
+using System.Windows.Forms;
+using OpenGL4NET;
+
+partial class MainForm : Form
+{
+	RenderingContext rc;
+	Random rnd;
+	int uhol = 0;
+
+	void MainFormLoad(object sender, EventArgs e)
+	{
+        double d = Math.Tan(60 * Math.PI / 360);
+        double r = 1.0 * ClientSize.Width / ClientSize.Height;
+        rc = RenderingContext.CreateContext(this);
+        gl.Viewport(0, 0, ClientSize.Width, ClientSize.Height);
+        gl.MatrixMode(GL.PROJECTION);
+        gl.Frustum(-d * r, d * r, -d, +d, 1, 1000);
+        gl.MatrixMode(GL.MODELVIEW);
+        gl.ClearDepth(1);
+        gl.Enable(GL.DEPTH_TEST);
+		rnd = new Random();
+	}
+	void Timer1Tick(object sender, EventArgs e)
+	{
+		gl.Clear(GL.COLOR_BUFFER_BIT | GL.DEPTH_BUFFER_BIT);
+		gl.LoadIdentity();
+		gl.PushMatrix(); 
+		gl.Translated(2, 2, -10);
+		gl.Rotated(uhol, 1, 0, 0);
+
+		gl.Begin(GL.TRIANGLES);
+		
+		//pravy horny
+
+		gl.Color3d(1, 0, 0);
+		gl.Vertex3d(0, 1, -10);
+		gl.Color3d(1, 0, -0);
+		gl.Vertex3d(-1,0,-10);
+		gl.Color3d(1, 0, 0);
+		gl.Vertex3d(1,0,-10);
+		
+//		//lavy dolny
+//		gl.Rotated(uhol, 0, 1, 0);
+//		gl.Color3d(1, 0, 0);
+//		gl.Vertex3d(-2, -2, -5);
+//		gl.Color3d(1, 0, -5);
+//		gl.Vertex3d(-1,-1,0);
+//		gl.Color3d(1, 0, -5);
+//		gl.Vertex3d(-2,-3,0);
+//		
+//		//pravy dolny
+//		gl.Rotated(uhol, 1, 0, 0);
+//		gl.Color3d(1, 0, 0);
+//		gl.Vertex3d(2, -2, -5);
+//		gl.Color3d(1, 0, -5);
+//		gl.Vertex3d(1,-3,0);
+//		gl.Color3d(1, 0, -5);
+//		gl.Vertex3d(3,-3,0);
+//		
+//		//lavy horny
+//		gl.Rotated(uhol, 1, 0, 0);
+//		gl.Color3d(1, 0, 0);
+//		gl.Vertex3d(-2, +2, -5);
+//		gl.Color3d(1, 0, -5);
+//		gl.Vertex3d(-3,+1,0);
+//		gl.Color3d(1, 0, -5);
+//		gl.Vertex3d(-1,+1,0);
+//		
+//		//stredny trojuholnik
+//		gl.Rotated(uhol, 0, 0, 1);
+//		gl.Color3d(1, 0, 0);
+//		gl.Vertex3d(-1, 0, -5);
+//		gl.Color3d(1, 0, -5);
+//		gl.Vertex3d(0,+1,0);
+//		gl.Color3d(0, 1, -5);
+//		gl.Vertex3d(0,3,0);
+	
+
+		gl.End();
+		gl.PopMatrix();
+		rc.SwapBuffers();
+		uhol = uhol + 10;
+	}
+	void MainFormPaint(object sender, PaintEventArgs e)
+	{
+
+	}
+}
