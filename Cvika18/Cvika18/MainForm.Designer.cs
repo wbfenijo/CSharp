b880e9cfa44940761f9a776204253442593b4adf
Adam <nestletycinka@gmail.com>
2025-12-03 23:00:48 +0100

    Task 18

---
+++ Cvika18/Cvika18/MainForm.Designer.cs
@@ -0,0 +1,62 @@
+﻿
+partial class MainForm
+{
+	private System.ComponentModel.IContainer components = null;
+	private System.Windows.Forms.Button button1;
+	private System.Windows.Forms.TextBox textBox1;
+
+	protected override void Dispose(bool disposing)
+	{
+		if (disposing && components != null) 
+		{
+			components.Dispose();
+		}
+		base.Dispose(disposing);
+	}
+
+	private void InitializeComponent()
+	{
+		this.button1 = new System.Windows.Forms.Button();
+		this.textBox1 = new System.Windows.Forms.TextBox();
+		this.SuspendLayout();
+		// 
+		// button1
+		// 
+		this.button1.Location = new System.Drawing.Point(392, 8);
+		this.button1.Name = "button1";
+		this.button1.Size = new System.Drawing.Size(75, 23);
+		this.button1.TabIndex = 0;
+		this.button1.Text = "button1";
+		this.button1.UseVisualStyleBackColor = true;
+		this.button1.Click += new System.EventHandler(this.Button1Click);
+		// 
+		// textBox1
+		// 
+		this.textBox1.Location = new System.Drawing.Point(424, 248);
+		this.textBox1.Multiline = true;
+		this.textBox1.Name = "textBox1";
+		this.textBox1.Size = new System.Drawing.Size(72, 48);
+		this.textBox1.TabIndex = 1;
+		this.textBox1.TextChanged += new System.EventHandler(this.TextBox1TextChanged);
+		// 
+		// MainForm
+		// 
+		this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+		this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+		this.ClientSize = new System.Drawing.Size(500, 300);
+		this.Controls.Add(this.textBox1);
+		this.Controls.Add(this.button1);
+		this.Name = "MainForm";
+		this.Text = "Cvika18";
+		this.Load += new System.EventHandler(this.MainFormLoad);
+		this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.MainFormMouseUp);
+		this.ResumeLayout(false);
+		this.PerformLayout();
+
+	}
+
+	public MainForm()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Cvika18/Cvika18/MainForm.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Drawing;
+using System.Windows.Forms;
+
+partial class MainForm : Form
+{
+	Vrchol hlava;
+	void TextBox1TextChanged(object sender, EventArgs e)
+	{
+	
+	}
+	void Button1Click(object sender, EventArgs e)
+	{
+//		Prvok prvy = new Prvok("Norbert", new Prvok("Adam", new Prvok("David", null)));
+//	    Prvok p;
+//	    p = new Prvok("Norbert", new Prvok("Adam", new Prvok("David", null)));
+//	    while (p != null )
+//	    {
+//	    	textBox1.AppendText(p.getValue());
+//	        textBox1.AppendText(Environment.NewLine);
+//	        p = p.getNext();
+//	    }
+
+
+		Graphics g = CreateGraphics();
+		hlava.kresli(g);
+
+
+	}
+	void MainFormLoad(object sender, EventArgs e)
+	{
+		hlava = new Vrchol(null, 150, 10);
+		Vrchol hrud = new Vrchol(hlava, 150, 30);
+		Vrchol rukaL = new Vrchol(hrud, 125, 40);
+		Vrchol rukaP = new Vrchol(hrud, 175, 40);
+		Vrchol brucho = new Vrchol(hrud, 150, 100);
+		Vrchol nohaL = new Vrchol(brucho, 125, 110);
+		Vrchol nohaP = new Vrchol(brucho, 175, 110);
+	}
+	void MainFormMouseUp(object sender, MouseEventArgs e)
+	{
+		hlava.najdi(e.X, e.Y);
+	}
+}
+class Prvok{
+	string value;
+	Prvok next;
+	
+	public Prvok(string ivalue, Prvok inext){
+		value = ivalue;
+		next = inext;
+	}
+	public string getValue(){
+		return value;
+	}
+	
+	public Prvok getNext(){
+		return next;
+	}
+
+}
+
+class Vrchol{
+	Vrchol parent;
+	List<Vrchol> children = new List<Vrchol>();
+	int pX;
+	int pY;
+	public Vrchol(Vrchol iparent, int ipX, int ipY){
+		parent = iparent;
+		if (parent != null){
+			parent.addChild(this);
+		}
+		pX = ipX;
+		pY = ipY;
+	}
+	
+	public Vrchol getParent(){
+		return parent;
+	}
+	
+	public void setParent(Vrchol iparent){
+		parent = iparent;
+	}
+	
+	public int getX(){
+		return pX;
+	}
+	
+	public void setX(int x){
+		pX = x;
+	}
+	
+	public void setY(int y){
+		pY = y;
+	}
+	
+	public int getY(){
+		return pY;
+	}
+	
+	public List<Vrchol> getChildren(){
+		return children;
+	}
+	
+	public void addChild(Vrchol child){
+		children.Add(child);
+	}
+	
+	public void kresli(Graphics g){
+		g.FillEllipse(Brushes.Black, pX - 5, pY - 5, 10, 10);
+		foreach (Vrchol child in children)
+		{
+			g.DrawLine(Pens.Black, pX, pY, child.getX(), child.getY());
+			child.kresli(g);
+		}
+//		for (int i = 0; i < children.Count; i++){
+//			g.DrawLine(Pens.Black, pX, pY, children[i].getX(), children[i].getY());
+//			children[i].kresli(g);
+//		}
+	}
+	
+	public Vrchol najdi(int x, int y){
+		if ((x > pX - 5 || x < pX + 5) && (y > pY - 5 || y < pY + 5)) return this;
+
+		foreach (Vrchol child in children)
+		{
+			if (x < child.getX() - 5 || x > child.getX() + 5) return null;
+			if (y < child.getY() - 5 || y > child.getY() + 5) return null;
+			return child;
+			
+		}
+		return null;
+	}
+}
